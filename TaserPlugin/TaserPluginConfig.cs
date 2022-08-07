c611c48fdda2c81338a8d9b3a1ec5387eaa2d242
Spxnkles <108006275+Spxnkles@users.noreply.github.com>
2022-08-07 14:10:49 +0200

    Slight changes to fix compatibilities for workshop

---
+++ TaserPlugin/TaserPluginConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+
+namespace TaserPlugin
+{
+    public class TaserPluginConfig : IRocketPluginConfiguration
+    {
+        public string TaserPerm;
+        public uint Taser;
+        public float TaseDuration;
+        public bool Debug;
+
+
+        public void LoadDefaults()
+        {
+            TaserPerm = "TaserPlugin.Tase";
+            Taser = 51200;
+            TaseDuration = 5;
+            Debug = true;
+
+        }
+    }
+}
